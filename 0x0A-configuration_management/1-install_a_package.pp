07b918e012c5b542d0c516e18e4649114bf700b8
hany0ageeb <hany0ageeb@gmail.com>
2024-02-23 02:46:22 -0800

    Task 1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# Using Puppet, install flask from pip3.
-# Requirements:
-#	1. Install flask
-#	2. Version must be 2.1.0
-#
+# Using Puppet, install flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
   provider => 'pip3',
 }
