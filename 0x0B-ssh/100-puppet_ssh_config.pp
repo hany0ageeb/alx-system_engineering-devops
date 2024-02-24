a47ea45b14f4dacf4827b5081e0d637e7fd79624
hany0ageeb <hany0ageeb@gmail.com>
2024-02-24 01:53:19 -0800

    Task 4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,20 @@
-# 4. Client configuration file (w/ Puppet)
-file { '/root/.ssh/config':
-  ensure  => 'file',
-  content => 'Host 100.26.241.78
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no',
+# 4. Client configuration file (w/ Puppet
+include stdlib
+file_line {'Host':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'Host 100.26.241.78',
+  replace => 'true',
+}
+file_line {'IdentityFile':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '	IdentityFile ~/.ssh/school',
+  replace => 'true',
+}
+file_line {'no password':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '	PasswordAuthentication no',
+  replace => 'true'
 }
