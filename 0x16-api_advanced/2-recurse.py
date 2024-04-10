21e593638a4cee7794431d9ea477564f270db5c0
hany0ageeb <hany0ageeb@gmail.com>
2024-04-10 04:14:05 -0700

    Task 2-recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
 """
 Write a recursive function that queries the Reddit API
-and returns a list containing the titles of all hot articles
-for a given subreddit. If no results are found for the given subreddit,
+and returns a list containing the titles of all
+hot articles for a given subreddit.
+If no results are found for the given subreddit,
 the function should return None.
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    """
-    queries the Reddit API and returns a list containing
-    the titles of all hot articles for a given subreddit
-    """
-    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+def recurse(subreddit, hot_list=[], after=None):
+    """Retrieves a list of hot posts from a given subreddit."""
     headers = {
-        'User-Agent': 'Custom'
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
     }
-    params = {
-        'after': after,
-        'limit': 100,
-    }
+    if after is None:
+        URL = 'https://www.reddit.com/r/{}/hot.json?limit=100'.format(
+                subreddit)
+    else:
+        URL = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'.format(
+                subreddit,
+                after)
     response = requests.get(
         URL,
         headers=headers,
-        params=params,
-        allow_redirects=False)
-    if response.status_code == 404:
-        return None
-    results = response.json().get('data')
-    after = results.get('after')
-    for itm in results.get('children'):
-        hot_list.append(itm.get('data').get('title'))
-    if after is not None:
-        recurse(subreddit, hot_list, after)
-    return hot_list
+        allow_redirects=False
+    )
+    if response.status_code != 200:
+        return hot_list if hot_list else None
+    data = response.json()['data']
+    hot_list.extend(list(map(lambda x: x['data']['title'], data['children'])))
+    after = data['after']
+    if after is None:
+        return hot_list if hot_list else None
+    return recurse(subreddit, hot_list, after)
